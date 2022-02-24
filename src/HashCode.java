b919e6de2ec6791e20127054747e103d2c87271f
Samet-Kervan <79086312+Samet-Kervan@users.noreply.github.com>
2022-02-24 19:35:48 +0300

    Read file created

---
+++ src/HashCode.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class HashCode {
+
+	private void readFile(String fileName) {
+		File file = new File(fileName);
+		try {
+			Scanner sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				String read = sc.nextLine();
+				String[] array = read.split(" ");
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File does not exists. Try after fixing the problem");
+			System.exit(0);
+		}
+	}
+}
